f582ca2dd380588a42667815ffe1d721705064c3
theimpossibleleap <122632859+theimpossibleleap@users.noreply.github.com>
2023-07-05 13:23:23 -0700

    Changed mobile sizing again

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -6,24 +6,24 @@ export default function Skills() {
         <>
             <div className="content-container"><h1>Skills</h1>
             <div id="skillsdiv">
-                <div className="skill"><h2 className="skills-text">Leadership</h2>&#9724; &#9724; &#9724; &#9724; &#9724;</div>
-                <div className="skill"><h2 className="skills-text">Problem Solving</h2>&#9724; &#9724; &#9724; &#9724; &#9724;</div>
-                <div className="skill"><h2 className="skills-text">Communication</h2>&#9724; &#9724; &#9724; &#9724; &#9724;</div>
-                <div className="skill"><h2 className="skills-text">Adaptability</h2>&#9724; &#9724; &#9724; &#9724; &#9724;</div>
-                <div className="skill"><h2 className="skills-text">Priority Setting</h2>&#9724; &#9724; &#9724; &#9724; &#9724;</div>
-                <div className="skill"><h2 className="skills-text">Detail Oriented</h2>&#9724; &#9724; &#9724; &#9724; &#9724;</div>
-                <div className="skill"><h2 className="skills-text">Collaboration</h2>&#9724; &#9724; &#9724; &#9724; &#9724;</div>
-                <div className="skill"><h2 className="skills-text">Continous Lerarning</h2>&#9724; &#9724; &#9724; &#9724; &#9724;</div>
-                <div className="skill"><h2 className="skills-text">Self-Motivator</h2>&#9724; &#9724; &#9724; &#9724; &#9724;</div>
-                <div className="skill"><h2 className="skills-text">HTML5</h2>&#9724; &#9724; &#9724; &#9724; &#9724;</div>
-                <div className="skill"><h2 className="skills-text">CSS3</h2>&#9724; &#9724; &#9724; &#9724; &#9723;</div>
-                <div className="skill"><h2 className="skills-text">VS Code</h2>&#9724; &#9724; &#9724; &#9724; &#9723;</div>
-                <div className="skill"><h2 className="skills-text">PM Software</h2>&#9724; &#9724; &#9724; &#9724; &#9723;</div>
-                <div className="skill"><h2 className="skills-text">JavaScript</h2>&#9724; &#9724; &#9724; &#9723; &#9723;</div>
-                <div className="skill"><h2 className="skills-text">Python</h2>&#9724; &#9724; &#9724; &#9723; &#9723;</div>
-                <div className="skill"><h2 className="skills-text">GitHub</h2>&#9724; &#9724; &#9724; &#9723; &#9723;</div>
-                <div className="skill"><h2 className="skills-text">SQL</h2>&#9724; &#9724; &#9724; &#9723; &#9723;</div>
-                <div className="skill"><h2 className="skills-text">React <small>(Learning)</small></h2>&#9724; &#9724; &#9723; &#9723; &#9723;</div>
+                <div className="skill"><h2 className="skills-text">Leadership</h2></div>
+                <div className="skill"><h2 className="skills-text">Problem Solving</h2></div>
+                <div className="skill"><h2 className="skills-text">Communication</h2></div>
+                <div className="skill"><h2 className="skills-text">Adaptability</h2></div>
+                <div className="skill"><h2 className="skills-text">Priority Setting</h2></div>
+                <div className="skill"><h2 className="skills-text">Detail Oriented</h2></div>
+                <div className="skill"><h2 className="skills-text">Collaboration</h2></div>
+                <div className="skill"><h2 className="skills-text">Continous Lerarning</h2></div>
+                <div className="skill"><h2 className="skills-text">Self-Motivator</h2></div>
+                <div className="skill"><h2 className="skills-text">HTML5</h2></div>
+                <div className="skill"><h2 className="skills-text">CSS3</h2></div>
+                <div className="skill"><h2 className="skills-text">VS Code</h2></div>
+                <div className="skill"><h2 className="skills-text">PM Software</h2></div>
+                <div className="skill"><h2 className="skills-text">JavaScript</h2></div>
+                <div className="skill"><h2 className="skills-text">Python</h2></div>
+                <div className="skill"><h2 className="skills-text">GitHub</h2></div>
+                <div className="skill"><h2 className="skills-text">SQL</h2></div>
+                <div className="skill"><h2 className="skills-text">React <small>(Learning)</small></h2>    </div>
             </div></div>
         </>
     );
