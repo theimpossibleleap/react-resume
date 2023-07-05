25c334806d41604af4e97ded6f237e8bf50cecb6
theimpossibleleap <122632859+theimpossibleleap@users.noreply.github.com>
2023-07-05 12:35:26 -0700

    Changed site title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ function Footer() {
             <div id="foot-container">
                 <div id="footer-text">
                     <h3>Contact Information</h3>
-                    <div className="footer-link"><small><i className="bi bi-envelope-fill">&nbsp;</i><a href="mailto:yell@adampunjabi.com">yell@adampunjabi.com</a></small></div>
-                    <div className="footer-link"><small><i className="bi bi-telephone-fill">&nbsp;</i><a href="">inquire through e-mail</a></small></div>
-                    <div className="footer-link"><small><i className="bi bi-linkedin">&nbsp;</i><a href="https://www.linkedin.com/in/adam-punjabi/">LinkedIn</a></small></div>
-                    <div className="footer-link"><small><i className="bi bi-geo-alt-fill">&nbsp;</i><a href="https://petapixel.com/assets/uploads/2023/06/PowersHeart_SCFgame5web-800x534.jpg" target="_blank">las vegas, nv</a></small></div>
+                    <div className="footer-link"><i className="bi bi-envelope-fill">&nbsp;</i><a href="mailto:yell@adampunjabi.com">yell@adampunjabi.com</a></div>
+                    <div className="footer-link"><i className="bi bi-telephone-fill">&nbsp;</i><a href="">inquire through e-mail</a></div>
+                    <div className="footer-link"><i className="bi bi-linkedin">&nbsp;</i><a href="https://www.linkedin.com/in/adam-punjabi/">LinkedIn</a></div>
+                    <div className="footer-link"><i className="bi bi-geo-alt-fill">&nbsp;</i><a href="https://petapixel.com/assets/uploads/2023/06/PowersHeart_SCFgame5web-800x534.jpg" target="_blank">las vegas, nv</a></div>
                 </div>
             </div>
         </div>
