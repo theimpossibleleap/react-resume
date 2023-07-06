9e40a3faaf3bc608d201b4f8a79b16b73c37911d
theimpossibleleap <122632859+theimpossibleleap@users.noreply.github.com>
2023-07-05 19:03:01 -0700

    Fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,6 @@ function Footer() {
                 <div id="footer-text">
                     <h3>Contact Information</h3>
                     <div className="footer-link"><i className="bi bi-envelope-fill">&nbsp;</i><a href="mailto:yell@adampunjabi.com">yell@adampunjabi.com</a></div>
-                    <div className="footer-link"><i className="bi bi-telephone-fill">&nbsp;</i><a href="">inquire through e-mail</a></div>
                     <div className="footer-link"><i className="bi bi-linkedin">&nbsp;</i><a href="https://www.linkedin.com/in/adam-punjabi/">LinkedIn</a></div>
                     <div className="footer-link"><i className="bi bi-geo-alt-fill">&nbsp;</i><a href="https://petapixel.com/assets/uploads/2023/06/PowersHeart_SCFgame5web-800x534.jpg" target="_blank">las vegas, nv</a></div>
                 </div>
